b3996ab6bfa083d393e1b88079df3876c304f361
siddq-dev <mhdsiddq17@gmail.com>
2026-07-21 11:41:21 +0530

    Added centralized routing system

---
+++ lib/routes/app_router.dart
@@ -0,0 +1,16 @@
+class AppRouter {
+  AppRouter._();
+
+  static const String home = '/';
+  static const String shop = '/shop';
+  static const String about = '/about';
+  static const String contact = '/contact';
+
+  static const String cart = '/cart';
+  static const String checkout = '/checkout';
+  static const String wishlist = '/wishlist';
+  static const String profile = '/profile';
+  
+  static const String login = '/login';
+  static const String register = '/register';
+}
