1faa6d4c1d2fe576e16a517b3b29fcac702db36e
siddq-dev <mhdsiddq17@gmail.com>
2026-07-22 11:20:09 +0530

    Added the each section in website layout

---
+++ lib/core/constants/section_sizes.dart
@@ -0,0 +1,15 @@
+class SectionSizes {
+  SectionSizes._();
+
+  static const double heroHeight = 600;
+
+  static const double categoriesHeight = 500;
+
+  static const double featuredCollectionHeight = 600;
+
+  static const double newArrivalsHeight = 600;
+
+  static const double whyChooseUsHeight = 500;
+
+  static const double newsletterHeight = 350;
+}
